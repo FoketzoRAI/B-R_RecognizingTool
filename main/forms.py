3e62ab72728b82c17a9decab60bd5e6be844d221
Dovhal Alexander <gribnik7676@gmail.com>
2021-07-31 19:38:59 +0300

    Added table 'Languages' and changed field language to FK connected to previous table.

--- main/forms.py
+++ main/forms.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm, forms, AuthenticationForm
 from django.contrib.auth.models import User
 
-from main.models import Bedrooms
+from main.models import Bedroom
 
 
 class RegisterUserForm(UserCreationForm):
-    username = forms.CharField(label='Логин-имя',widget=forms.TextInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(label='Логин-имя', widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
@@ -14,11 +14,13 @@ class RegisterUserForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
+
 class LoginUserForm(AuthenticationForm):
     username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
+
 class BedroomsForm(forms.ModelForm):
     class Meta:
-        model = Bedrooms
+        model = Bedroom
         fields = ['keywords']
--- main/models.py
+++ main/models.py
@@ -1,7 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
-class Bedrooms(models.Model):
-    language = models.CharField(max_length=30, blank=True)
-    description = models.TextField()
-    keywords = models.CharField(max_length=255, blank=True)
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Язык")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ''' Model settings '''
+        verbose_name = 'Язык'
+        verbose_name_plural = 'Языки'
+        ordering = ['name']
+
+
+class Bedroom(models.Model):
+    language = models.ForeignKey('Language', on_delete=models.PROTECT, null=True, verbose_name="Язык")
+    description = models.TextField(verbose_name="Описание")
+    keywords = models.CharField(max_length=255, blank=True, verbose_name="Ключевые слова")
+
+    class Meta:
+        ''' Model settings '''
+        verbose_name = 'Спальня'
+        verbose_name_plural = 'Спальни'
