088c2e7f7a23704a43bdf3c4b0a5d7f3d1d79aca
Dovhal Alexander <gribnik7676@gmail.com>
2021-07-31 20:15:01 +0300

    Were configured the models data showing on admin panel such as 'Bedroom' & 'Language' + added 'Main' app verbose name 'Главная'

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Language, Bedroom
 # Register your models here.
+
+
+class LanguageAdmin(admin.ModelAdmin):
+    ''' Language data on admin page '''
+    list_display = ('id', 'name')
+    list_display_links = ('name',)
+
+
+class BedroomAdmin(admin.ModelAdmin):
+    ''' Bedroom data on admin page '''
+    list_display = ('id', 'language', 'description', 'keywords')
+    list_display_links = ('id',)
+    search_fields = ('description', 'keywords')
+
+
+admin.site.register(Language, LanguageAdmin)
+admin.site.register(Bedroom, BedroomAdmin)
